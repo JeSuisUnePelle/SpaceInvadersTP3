4000370a602a3d603ba83c58f656f06f3d465e97
JeSuisUnePelle <samuel.villeneuve@cegepsi.ca>
2023-05-01 14:59:24 -0400

    j'ai fait un menu mais maintenant, lorsque je tire sur les ennemis , la balle passe a travers et lorsqu'il me tire dessus, la balle passe a travers aussi.

--- src/main/java/com/example/spaceinvaderstp3/SpaceInvaders.java
+++ src/main/java/com/example/spaceinvaderstp3/SpaceInvaders.java
@@ -4,12 +4,22 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
+
+
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +29,10 @@ import java.io.IOException;
 public class SpaceInvaders extends Application
 {
     private Pane racine = new Pane();
+    private boolean jeuCommence = false;
+
+    private Pane jeuRacine;
+    private Stage primaryStage;
 
     private double temps = 0;
 
@@ -26,9 +40,10 @@ public class SpaceInvaders extends Application
 
     private Parent creationDeContenu()
     {
-        racine.setPrefSize(1000, 1000);
+        jeuRacine = new Pane();
+        jeuRacine.setPrefSize(1000, 1000);
 
-        racine.getChildren().add(Joueur);
+        jeuRacine.getChildren().add(Joueur);
 
         // Va rendre les animation plus fluide
         AnimationTimer minuteur = new AnimationTimer()
@@ -42,26 +57,87 @@ public class SpaceInvaders extends Application
 
         minuteur.start();
 
-        prochainNiveau();
+        return jeuRacine;
+    }
 
-        return racine;
+    private Parent createMenu() {
+        VBox menu = new VBox(10);
+        menu.setPrefSize(1000, 1000);
+        menu.setAlignment(Pos.CENTER);
+
+        Button startButton = new Button("Commencer la partie");
+        startButton.setOnAction(e ->
+        {
+            redemarrerJeu();
+            Scene scene = new Scene(jeuRacine);
+            scene.setOnKeyPressed(keyEvent -> {
+                switch (keyEvent.getCode()) {
+                    case A:
+                        Joueur.versLaGauche();
+                        break;
+                    case D:
+                        Joueur.versLaDroite();
+                        break;
+                    case SPACE:
+                        tir(Joueur);
+                        break;
+                    case R:
+                        redemarrerJeu();
+                        break;
+                }
+            });
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        });
+
+
+        Button quitButton = new Button("Quitter");
+        quitButton.setOnAction(e -> {
+            primaryStage.close();
+        });
+
+        menu.getChildren().addAll(startButton, quitButton);
+
+        Pane menuPane = new Pane(menu);
+        menuPane.setPrefSize(1000, 1000);
+        return menuPane;
+    }
+
+
+    private Button createRestartButton()
+    {
+        Button restartButton = new Button("Recommencer");
+        restartButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                racine.getChildren().clear();
+                racine.getChildren().add(Joueur);
+                Joueur.mort = false;
+                prochainNiveau();
+            }
+        });
+
+        return restartButton;
     }
 
     private void prochainNiveau()
     {
         for (int i = 0; i < 5; i++)
         {
-            Sprite s = new Sprite(90 + i*100, 150, 30, 30, "ennemi", Color.RED);
-
-            racine.getChildren().add(s);
+            Sprite s = new Sprite(90 + i * 100, 150, 30, 30, "ennemi", Color.RED);
+            jeuRacine.getChildren().add(s);
         }
     }
 
 
 
+
+
     private List<Sprite> sprites()
     {
-        return racine.getChildren().stream()
+        return jeuRacine.getChildren().stream()
                 .filter(n -> n instanceof Sprite)
                 .map(n -> (Sprite) n)
                 .collect(Collectors.toList());
@@ -115,16 +191,16 @@ public class SpaceInvaders extends Application
 
         racine.getChildren().removeIf(n -> n instanceof Sprite && ((Sprite) n).mort);
 
+        if (jeuCommence && toutLesEnnemisDetruits()) {
+            displayVictoryMessage();
+        }
 
         if (temps > 2)
         {
             temps = 0;
         }
 
-        if (toutLesEnnemisDetruits())
-        {
-            displayVictoryMessage();
-        }
+
 
         if (Joueur.mort)
         {
@@ -134,54 +210,81 @@ public class SpaceInvaders extends Application
 
     private void displayVictoryMessage()
     {
-        Text victoryMessage = new Text("VOUS AVEZ GAGNÉ !");
+        Text victoryMessage = new Text("VOUS AVEZ GAGNÉ ! Appuyez sur R pour recommencer.");
         victoryMessage.setFont(Font.font("Verdana", 50));
         victoryMessage.setFill(Color.GREEN);
-        victoryMessage.setX(racine.getPrefWidth() / 2 - victoryMessage.getLayoutBounds().getWidth() / 2);
-        victoryMessage.setY(racine.getPrefHeight() / 2);
-        racine.getChildren().add(victoryMessage);
+
+        VBox vbox = new VBox(10, victoryMessage);
+        vbox.setAlignment(Pos.CENTER);
+
+        Pane messagePane = new Pane();
+        messagePane.setPrefSize(jeuRacine.getPrefWidth(), jeuRacine.getPrefHeight());
+        messagePane.getChildren().add(vbox);
+        vbox.setLayoutX(jeuRacine.getPrefWidth() / 2 - vbox.getPrefWidth() / 2);
+        vbox.setLayoutY(jeuRacine.getPrefHeight() / 2 - vbox.getPrefHeight() / 2);
+
+        jeuRacine.getChildren().add(messagePane);
     }
 
-    private void displayDefeatMessage() {
-        Text defeatMessage = new Text("VOUS AVEZ PERDU !");
+    private void displayDefeatMessage()
+    {
+        Text defeatMessage = new Text("VOUS AVEZ PERDU ! Appuyez sur R pour recommencer.");
         defeatMessage.setFont(Font.font("Verdana", 50));
         defeatMessage.setFill(Color.RED);
-        defeatMessage.setX(racine.getPrefWidth() / 2 - defeatMessage.getLayoutBounds().getWidth() / 2);
-        defeatMessage.setY(racine.getPrefHeight() / 2);
-        racine.getChildren().add(defeatMessage);
+
+        VBox vbox = new VBox(10, defeatMessage);
+        vbox.setAlignment(Pos.CENTER);
+
+        Pane messagePane = new Pane();
+        messagePane.setPrefSize(jeuRacine.getPrefWidth(), jeuRacine.getPrefHeight());
+        messagePane.getChildren().add(vbox);
+        vbox.setLayoutX(jeuRacine.getPrefWidth() / 2 - vbox.getPrefWidth() / 2);
+        vbox.setLayoutY(jeuRacine.getPrefHeight() / 2 - vbox.getPrefHeight() / 2);
+
+        jeuRacine.getChildren().add(messagePane);
     }
+
+
+    private void redemarrerJeu() {
+        jeuRacine.getChildren().clear();
+        Joueur = new Sprite(300, 750, 40, 40, "Joueur", Color.BLUE);
+        jeuRacine.getChildren().add(Joueur);
+        prochainNiveau();
+        jeuCommence = true; // Cette ligne est déjà présente
+    }
+
+
+
 
     private void tir(Sprite qui)
     {
-        Sprite s = new Sprite((int) qui.getTranslateX() + 20, (int) qui.getTranslateY(), 5, 20, qui.type + "Tir", Color.PURPLE);
+        int offsetX = 20;
+        int offsetY = 20;
 
-        racine.getChildren().add(s);
+        if (qui.type.equals("ennemi")) {
+            offsetY = -offsetY;
+        }
+
+        Sprite s = new Sprite((int) qui.getTranslateX() + offsetX, (int) qui.getTranslateY() + offsetY, 5, 20, qui.type + "Tir", Color.PURPLE);
+
+        jeuRacine.getChildren().add(s);
     }
+
+
 
     @Override
     public void start(Stage stage) throws Exception
     {
-        Scene scene = new Scene(creationDeContenu());
+        this.primaryStage = stage;
 
-        scene.setOnKeyPressed(e ->
-        {
-            switch (e.getCode())
-            {
-                case A:
-                    Joueur.versLaGauche();
-                    break;
-                case D:
-                    Joueur.versLaDroite();
-                    break;
-                case SPACE:
-                    tir(Joueur);
-                    break;
-            }
-        });
+        Scene menuScene = new Scene(createMenu());
+        primaryStage.setScene(menuScene);
+        primaryStage.show();
 
-        stage.setScene(scene);
-        stage.show();
+        jeuRacine = (Pane) creationDeContenu();
     }
+
+
 
     private boolean toutLesEnnemisDetruits()
     {
